d45caa7780624e6ea3f5d84d333d3272e731dc8d
DavidCRgh <dval7788@gmail.com>
2025-06-03 18:29:35 -0600

    Add setup.py to install mmcls as a package

---
+++ mmcls/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='mmcls',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'mmcv',
+    ],
+    python_requires='>=3.8',
+    author='David Valverde',
+    description='MMClassification package for deep learning',
+    license_files=['LICENSE'],
+    include_package_data=True,
+)
