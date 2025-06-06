14594fcc1df994203b772f2848836f8306d98120
DavidCRgh <dval7788@gmail.com>
2025-06-05 20:12:43 -0600

    Fix mmcls setup.py and missing init file in vit submodule

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='mmcls',
     version='0.1.0',
-    packages=['mmcls'],
-    package_dir={'mmcls': '.'},
+    packages=find_packages(),
     install_requires=[
         'mmcv',
     ],
